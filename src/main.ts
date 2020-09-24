c686ca32585c1a7279e7941b45200527d80dc87a
Your Name <you@example.com>
2020-09-24 13:07:00 +0200

    desde equipo departamento con callback

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,5 @@ let multiplicar: (a: number, b: number, fun: (r: number) => void)
 
 multiplicar = (a, b, callbackwu) => callbackwu(a*b)
 
+// Una función que en r recibe el resultado de la función definida
 multiplicar(3, 4, (r) => console.log(`Resutado: ${r}`)    )
