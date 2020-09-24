a8a8aed0b142c26a492ce23e0a9aa62c961530be
asalsan790 <asalto@iespuntadelverde.es>
2020-09-24 18:57:54 +0200

    ejemplo callback

---
+++ src/ejemplosCallback.ts
@@ -0,0 +1,114 @@
+// Ejemplos de callback: función que se pasa como argumento a otra función
+// Ideas de:
+// https://altocodigo.blogspot.com/2019/07/funciones-callback-en-javascript.html
+
+/// Ejemplo 1 
+// ponemos a la función tipo any por simplificar la notación de tipos
+ 
+function main01(callback01: any){  
+    console.log('1º Buenos días');  
+    callback01();  
+}  
+function cafe(){  
+    console.log('2º ¿Quieres un cafe?');  
+} 
+main01(cafe); 
+
+// Ejemplo 1 con otra notación sin la palabra function
+// En este caso expresamos el tipo del parámetro callback01: 
+//      función que no recibe nada y no devuelve nada
+
+let main02 = (callback01: () => void) => {  
+    console.log('3º Buenos días');  
+    callback01();  
+}  
+let cafe2 = () => console.log('4º ¿Quieres un cafe?');  
+
+main02(cafe2); 
+
+// En esta variante del Ejemplo 1 utilizo una función anónima
+// invoco a la función main03 pasándole una función,
+// main03 hace priemro su trabajo que es escribir el pimer mensaje 
+// y después invoca a la función que le he pasado.
+
+function main03(callback: any){  
+    console.log('5º Buenos días');  
+    callback();  
+} 
+main03( function() {  
+    console.log('6º ¿Quieres un cafe?');  
+});  
+
+
+// Con otra notación
+
+let main04 = (callback: any) => {  
+    console.log('7º Buenos días');  
+    callback();  
+} 
+main04( () => {  
+    console.log('8º ¿Quieres un cafe?');  
+    }
+);  
+
+// Ejemplo 2 Pasando un parámetro a la función callback
+
+function main05(callback: any){  
+    let nombre = 'Andrés Pérez López';  
+    callback(nombre);  
+}  
+function saluda(nombre: any){  
+    console.log('Hola '+ nombre);  
+}  
+main05(saluda);  
+
+// Ejemplo 2 con otra notación
+
+let main06 = (callback: any) => {
+    let miNombre = 'Santiago Segura';  
+    callback(miNombre);  
+}
+let saluda01 = (nombre: string) => console.log('Qué tal '+ nombre);
+main06(saluda01)
+
+
+// Ahora la función callback la hacemos anónima
+
+let main07 = (callback: any) => {
+    let miNombre = 'Antonio López';  
+    callback(miNombre);  
+}
+
+main07( (nombre: string) => 
+        console.log('Qué tal estás'+ nombre) 
+    )
+
+// Nos vamos al penúltimo ejemplo
+
+function main08 (a: number, b: number, callback: any){  
+    let total = a+b;  
+    callback(total);  
+   }  
+main08(2, 3, function(t: number) {  
+    if(t>=5){  
+     console.log("Prueba superada: " + t);  
+    }else{  
+     console.log("Inténtalo de nuevo: " + t);  
+    }  
+   }
+);  
+
+///////// Con otra notación
+
+function main09(a: number, b: number, callback: (r: number) => void){  
+    var total = a + b;  
+    callback( total );  
+}  
+main09(2, 2, (t) => {  
+        if(t >= 5){  
+            console.log("Prueba superada: " + t);  
+        }else{  
+            console.log("Inténtalo de nuevo: " + t);  
+        }  
+    }
+); 
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,34 @@ import Producto from './producto'
 // hacemos otro cambio para ver el correo del autor
 var miPro = new Producto()
 
-let multiplicar: (a: number, b: number, fun: (r: number) => void) 
-            => void
+// Defino el prototipo o tipo de dato con los tipos de datos utilizados
+let multiplicar: ( a: number, b: number, fun: (r: number) => void ) 
+                    => void
 
-multiplicar = (a, b, callbackwu) => callbackwu(a*b)
+multiplicar = (a, b, f) => a*b
 
 // Una función que en r recibe el resultado de la función definida
-multiplicar(3, 4, (r) => console.log(`Resutado: ${r}`)    )
+multiplicar(3, 4, (r) => console.log( `Resutado: ${r}`)    )
+
+function sumar(a: number, b: number): number {
+    return (a+b) 
+}
+
+let sumar2 = (a: number, b: number): number => (a+b) 
+console.log(sumar2(7,9))
+
+let sumar3 = (a:number, b: number, f:(r:number) => void) => {
+    
+}
+
+
+function saludar(nombre: string) {
+    console.log('Hola ' + nombre);
+}
+  
+function procesarEntradaUsuario( callback_f: (v: string) => void ) {
+    let nombre = "Antonio López Pérez";
+    callback_f(nombre);
+}
+  
+procesarEntradaUsuario( saludar );
