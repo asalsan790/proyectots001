ca103d31b528fac7868ec2ffeb137f2369088453
Your Name <you@example.com>
2020-09-24 11:12:25 +0200

    desde equipo dpato

--- src/producto.ts
+++ src/producto.ts
@@ -1,4 +1,5 @@
 // hacemos un export default para poder importar desde otro archivo
+// Cambio en la nueva carpeta creada con clone
 export default class Producto{
     constructor() {
         console.log("soy un objeto producto para empezar")
