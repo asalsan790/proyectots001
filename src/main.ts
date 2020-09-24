6e7132bed433c6d4b43466b78f51f03a901cd793
Your Name <you@example.com>
2020-09-24 13:02:06 +0200

    desde equipo departamento con callback

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,10 @@ import Producto from './producto'
 // Observa que importo una clase desde otro archivo.
 // hacemos otro cambio para ver el correo del autor
 var miPro = new Producto()
+
+let multiplicar: (a: number, b: number, fun: (r: number) => void) 
+            => void
+
+multiplicar = (a, b, callbackwu) => callbackwu(a*b)
+
+multiplicar(3, 4, (r) => console.log(`Resutado: ${r}`)    )
